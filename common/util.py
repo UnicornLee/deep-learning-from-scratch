6960708312aa6b21ef29ac51dcea19770c9e89ee
UnicornLee <1571418313@qq.com>
2025-03-30 16:22:55 +0800

    第7章 卷积神经网络

--- common/util.py
+++ common/util.py
@@ -28,3 +28,77 @@ def shuffle_dataset(x, t):
     t = t[permutation]
 
     return x, t
+
+
+def conv_output_size(input_size, filter_size, stride=1, pad=0):
+    """计算卷积层的输出大小
+
+    Args:
+        input_size: 输入大小（高x宽）
+        filter_size: 滤波器大小（高x宽）
+        stride: 步长
+        pad: 填充
+
+    Returns:
+        输出大小
+    """
+    return (input_size + 2*pad - filter_size) / stride + 1
+
+
+def im2col(input_data, filter_h, filter_w, stride=1, pad=0):
+    """将输入的图像进行展开
+
+    Arg:
+        input_data: 由（数据量，通道，高，宽）的4维数组构成的输入数据
+        filter_h: 滤波器的高
+        filter_w: 滤波器的宽
+        stride: 步长
+        pad: 填充
+
+    Returns:
+        col: 二维数组
+    """
+    N, C, H, W = input_data.shape
+    out_h = (H + 2*pad - filter_h)//stride + 1
+    out_w = (W + 2*pad - filter_w)//stride + 1
+
+    img = np.pad(input_data, [(0,0), (0,0), (pad, pad), (pad, pad)], 'constant')
+    col = np.zeros((N, C, filter_h, filter_w, out_h, out_w))
+
+    for y in range(filter_h):
+        y_max = y + stride*out_h
+        for x in range(filter_w):
+            x_max = x + stride*out_w
+            col[:, :, y, x, :, :] = img[:, :, y:y_max:stride, x:x_max:stride]
+
+    col = col.transpose(0, 4, 5, 1, 2, 3).reshape(N*out_h*out_w, -1)
+    return col
+
+
+def col2im(col, input_shape, filter_h, filter_w, stride=1, pad=0):
+    """卷积层参数的逆展开
+
+    Arg:
+        col: 二维数组
+        input_shape: 输入数据的形状（例如：（10,1,28,28））
+        filter_h: 滤波器的高
+        filter_w: 滤波器的宽
+        stride: 步长
+        pad: 填充
+
+    Returns:
+
+    """
+    N, C, H, W = input_shape
+    out_h = (H + 2*pad - filter_h)//stride + 1
+    out_w = (W + 2*pad - filter_w)//stride + 1
+    col = col.reshape(N, out_h, out_w, C, filter_h, filter_w).transpose(0, 3, 4, 5, 1, 2)
+
+    img = np.zeros((N, C, H + 2*pad + stride - 1, W + 2*pad + stride - 1))
+    for y in range(filter_h):
+        y_max = y + stride*out_h
+        for x in range(filter_w):
+            x_max = x + stride*out_w
+            img[:, :, y:y_max:stride, x:x_max:stride] += col[:, :, y, x, :, :]
+
+    return img[:, :, pad:H + pad, pad:W + pad]
